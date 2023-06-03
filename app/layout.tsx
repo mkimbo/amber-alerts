268a5e540cdd5ea0546745d74d7b3fc688e08fa4
Jack <jackmkimbo@gmail.com>
2023-06-03 12:40:37 +0300

    metadata issue trials

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,52 @@
-//import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Navbar } from "./Components/Navbar";
 import ToastProvider from "./Components/ToastProvider";
 import "./globals.scss";
 import styles from "./layout.module.scss";
 import { Suspense } from "react";
 
-// add next js 13 meta data
-
-// export const metadata: Metadata = {
-// title: "Missing Link",
-// openGraph: {
-//   title: 'Next.js',
-//   description: 'The React Framework for the Web',
-//   url: 'https://nextjs.org',
-//   siteName: 'Next.js',
-//   images: [
-//     {
-//       url: 'https://nextjs.org/og.png',
-//       width: 800,
-//       height: 600,
-//     },
-//     {
-//       url: 'https://nextjs.org/og-alt.png',
-//       width: 1800,
-//       height: 1600,
-//       alt: 'My custom alt',
-//     },
-//   ],
-//   locale: 'en_US',
-//   type: 'website',
-// },
-//   robots: {
-//     index: true,
-//     follow: true,
-//     googleBot: {
-//       index: true,
-//       follow: true,
-//       "max-video-preview": -1,
-//       "max-image-preview": "large",
-//       "max-snippet": -1,
-//     },
-//   },
-//   verification: {
-//     google: "VaD1qjKK95G1B1wsA3ZydoAdSg2r3aCm6D7ZJw2bw",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Missing Link",
+  description:
+    "Proximity-based community alerts system for missing persons and vehicles",
+  openGraph: {
+    title: "Missing Link",
+    description:
+      "Proximity-based community alerts system for missing persons and vehicles",
+    url: "https://amber-alerts.vercel.app/",
+    siteName: "Missing Link",
+    images: [
+      {
+        url: "https://leerob.io/og.jpg", // Replace this with your own image
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Missing Link",
+    card: "summary_large_image",
+  },
+  // icons: {
+  //   shortcut: "/favicon.ico",
+  // },
+  verification: {
+    google: "VaD1qjKK95G1B1wsA3ZydoAdSg2r3aCm6D7ZJw2bw",
+  },
+};
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,8 @@
-import { Metadata } from "next";
 import styles from "./page.module.scss";
 import { VscBroadcast } from "react-icons/vsc";
 export async function generateStaticParams() {
   return [{}];
 }
-
-export const metadata: Metadata = {
-  title: "Missing Link",
-  description:
-    "Proximity-based community alerts system for missing persons and vehicles",
-  openGraph: {
-    title: "Missing Link",
-    description:
-      "Proximity-based community alerts system for missing persons and vehicles",
-    url: "https://amber-alerts.vercel.app/",
-    siteName: "Missing Link",
-    images: [
-      {
-        url: "https://leerob.io/og.jpg", // Replace this with your own image
-        width: 1920,
-        height: 1080,
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: "Missing Link",
-    card: "summary_large_image",
-  },
-  // icons: {
-  //   shortcut: "/favicon.ico",
-  // },
-  verification: {
-    google: "VaD1qjKK95G1B1wsA3ZydoAdSg2r3aCm6D7ZJw2bw",
-  },
-};
 
 export default function Home() {
   return (
