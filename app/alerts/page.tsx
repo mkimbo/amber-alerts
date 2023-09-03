684fc74265c5701eb557378d717a336e042ac36f
Jack <jackmkimbo@gmail.com>
2023-09-03 09:40:38 +0300

    minor updates

--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -1,14 +1,15 @@
 import styles from "./page.module.scss";
 import { ServerAuthProvider } from "../../auth/server-auth-provider";
 import Link from "next/link";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowRight, MdPersonPin } from "react-icons/md";
+import { AiFillCar } from "react-icons/ai";
 //import { VerifyForm } from "./VerifyForm";
 
 export default function Alerts() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h2 className={styles.title}>Alerts</h2>
+        <h2 className={styles.header}>Alerts</h2>
         {/* @ts-expect-error https://github.com/vercel/next.js/issues/43537 */}
         <ServerAuthProvider>
           <div>
@@ -17,6 +18,41 @@ export default function Alerts() {
             property. Your accurate and detailed information can make a
             significant difference in the search and recovery process.{" "}
           </div>
+          <div className={styles.mainLinks}>
+            <Link href={`/new/alert?type=person`} className={styles.card}>
+              <MdPersonPin
+                className={styles.icon}
+                color={"#ff4400"}
+                fontSize={40}
+              />
+              <div className={styles.cardRight}>
+                <div className={styles.info}>
+                  <span className={styles.title}>Missing Person</span>
+                  <div className={styles.description}>
+                    All users near you will receive this alert
+                  </div>
+                </div>
+                <MdKeyboardArrowRight fontSize={35} className={styles.arrow} />
+              </div>
+            </Link>
+            <Link href={`/new/alert?type=motor`} className={styles.card}>
+              <AiFillCar
+                className={styles.icon}
+                color={"#ff4400"}
+                fontSize={40}
+              />
+              <div className={styles.cardRight}>
+                <div className={styles.info}>
+                  <span className={styles.title}>Missing Vehicle/Bike</span>
+                  <div className={styles.description}>
+                    Send alert to subscribed users near you
+                  </div>
+                </div>
+                <MdKeyboardArrowRight fontSize={35} className={styles.arrow} />
+              </div>
+            </Link>
+          </div>
+          {/* 
           <Link href={`/new/alert?type=person`} className={styles.btn}>
             <span className={styles.report}>Missing Person</span>{" "}
             <MdKeyboardArrowRight fontSize={25} />
@@ -24,7 +60,7 @@ export default function Alerts() {
           <Link href={`/new/alert?type=motor`} className={styles.btn}>
             <span className={styles.report}>Missing Vehicle/Bike</span>{" "}
             <MdKeyboardArrowRight fontSize={25} />
-          </Link>
+          </Link> */}
         </ServerAuthProvider>
       </div>
     </div>
